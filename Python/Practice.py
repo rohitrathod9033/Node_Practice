d1745285b396a546103042b8cb68cffbf31eca73
Rohit Rathod <rohitrathod60371@gmail.com>
2025-08-07 06:06:39 +0530

    Push

--- Python/Practice.py
+++ Python/Practice.py
@@ -1,5 +1,11 @@
-print("Hello World")
+# print("Hello World")
 
-name = "Rohit"
-Age = 22
+# name = "Rohit"
+# Age = 22
 
+# a = 12
+# b = 15
+# print(name.upper())
+# print(name.lower())
+# print(name.find('R'))
+# print(name.replace("Rohit", "IAS Rohit Rathod"))
