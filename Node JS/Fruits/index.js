cbd9f9765d4e9002bd2d6c198ef60d00441a8bbe
Rohit Rathod <rohitrathod60371@gmail.com>
2025-08-09 19:32:28 +0530

    Last

--- Node JS/Fruits/index.js
+++
@@ -1,7 +0,0 @@
-// Baki File se data leti haiii like App.jsx
-
-const Apple = require('./Apple')
-const Mango = require('./Mango')
-const Orange = require('./Orange')
-
-module.exports = [ Apple, Mango, Orange ];
